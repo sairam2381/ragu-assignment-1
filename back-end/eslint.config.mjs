a734c5ac872037dd9990ce620638816273f3757a
Sreesairam V <sairam2381@users.noreply.github.com>
2025-05-24 14:29:01 +0530

    the changes in the frontend are done

--- back-end/eslint.config.mjs
+++ back-end/eslint.config.mjs
@@ -1,11 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import eslintPluginJs from "@eslint/js";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    ignores: ["node_modules/**"],
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    plugins: {},
+    rules: {
+      ...eslintPluginJs.configs.recommended.rules,
+    },
+  },
+];
